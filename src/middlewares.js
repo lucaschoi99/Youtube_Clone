981ea208b31630aedc04c443e796dd6a8a12aed9
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-02-24 19:24:01 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,19 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+    credentials: {
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET
+    }
+});
+
+const multerUploader = multerS3({
+    s3: s3,
+    bucket: 'youtubecloneproject',
+    acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
     // Set locals
@@ -33,9 +48,11 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUploadMiddleware = multer({
     dest: "uploads/avatars/",
     limits: { fileSize: 5000000, },
+    storage: multerUploader,
 });
 
 export const videoUploadMiddleware = multer({
     dest: "uploads/videos/",
     limits: { fileSize: 500000000, },
-})
+    storage: multerUploader,
+});
